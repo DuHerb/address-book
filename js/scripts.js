c307e01c00f97ac38556366ea2451da2eefbc9ea
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-04-09 21:59:49 -0700

    add form inputs and functions to insert a newaddress object into a contact object

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function AddressBook() {
   this.currentId = 0
 }
 
-AddressBook.prototype.addContact = function (contact) {
+AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts.push(contact);
 };
@@ -38,21 +38,25 @@ AddressBook.prototype.deleteContact = function(id) {
 }
 
 //Business Logic for Contacts --------------------------------------//
-function Contact(firstName, lastName, phoneNumber, email) {
+function Contact(firstName, lastName) {
   this.firstName = firstName,
   this.lastName = lastName,
-  this.phoneNumber = phoneNumber,
-  this.email = email
+  this.addressList = []
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+Contact.prototype.addAddress = function(address) {
+  this.addressList.push(address);
+}
+
 //Business Logic for Addresses
-function Address(type, email, street, city, zip) {
+function Address(type, email, phone, street, city, zip) {
   this.type = type,
   this.email = email,
+  this.phoneNumber = phone,
   this.street = street,
   this.city = city,
   this.zip = zip
@@ -103,16 +107,27 @@ $(document).ready(function(){
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedType = $("#new-address-type").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var inputtedEmail = $("input#new-email").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedZip = $("input#new-zip").val();
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $("input#new-email").val("");
+    $("#input#new-address-type").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-zip").val("");
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
     addressBook.addContact(newContact);
+    var newAddress = new Address(inputtedType, inputtedEmail, inputtedPhoneNumber, inputtedStreet, inputtedCity, inputtedZip);
+    newContact.addAddress(newAddress);
+    console.log(addressBook);
     displayContactDetails(addressBook);
   })
   //end form submit -------------------------------------------------//
